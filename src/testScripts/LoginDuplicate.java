5eafc6336c894f334cfa43e6e4caa9029d7f8d16
Saurav Kumar <saurav.kumar@sophos.com>
2017-01-02 11:01:43 +0530

    Final Commit

---
+++ src/testScripts/LoginDuplicate.java
@@ -0,0 +1,16 @@
+package testScripts;
+import org.testng.annotations.Test;
+import config.Driver;
+import pageObjects.LoginPage;
+import testBase.TestBase;
+import utilities.Constants;
+
+public class LoginDuplicate extends TestBase {
+	
+  @Test
+  public void ValidateLogin() {
+	  
+	  System.out.println("In second classs!!!!");
+  }
+  
+}
