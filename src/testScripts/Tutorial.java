ded1e64f889fca7081f4fb9e7538fabf43ad2bd5
Saurav Kumar <saurav.kumar@sophos.com>
2017-03-06 08:43:00 +0530

    IntelliJ commit

--- src/testScripts/Tutorial.java
+++ src/testScripts/Tutorial.java
@@ -1,36 +1,31 @@
 package testScripts;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Stack;
-import java.util.TreeMap;
-import java.util.TreeSet;
-
-import utilities.ReadWriteExcel;
-import utilities.XMLParser;
-
-public class Tutorial {
-
-	public static void main(String[] args) throws IOException {
-		 LinkedHashMap<Integer,String> hm = new LinkedHashMap<Integer,String>();  
-		  
-		  hm.put(100,"Saurav");  
-		  hm.put(102,"Tushar");  
-		  hm.put(101,"Sneh");  
-		  hm.put(103,"Avinash");  
-		  
-		  for(Map.Entry m:hm.entrySet()){  
-		   System.out.println(m.getKey()+" "+m.getValue());  
-		  }  
-		  
-	  }
+interface Mobile
+{
+	void call();
+	void message();
 }
+
+class BlackDuck implements Mobile
+{
+	public void call()
+	{
+		System.out.println("Calling");
+	}
+
+	public void message()
+	{
+		System.out.println("Messaging");
+	}
+}
+
+public class Tutorial
+{
+	public static void main(String args[])
+	{
+		BlackDuck obj=new BlackDuck();
+		obj.call();
+		obj.message();
+	}
+}
+
